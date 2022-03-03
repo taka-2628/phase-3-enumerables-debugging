146de685b7d36bd28e0732c98a7aafd6cb6021b8
Takashi <th2826@columbia.edu>
2022-03-03 12:29:37 -0500

    Splitting the String

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,10 @@
 # Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+  binding.pry
+  puts words
+end
+
+reverse_each_word("Hello there, and how are you?")
